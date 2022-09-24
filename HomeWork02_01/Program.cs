f863d9b17c8135362e9dfc78b4ed1389a7601202
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-24 22:05:00 +0300

    Вторая цифра из трехзначного числа

---
+++ HomeWork02_01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (99 < number && number < 1000)
+{
+    int g = (number/10)%10;
+    Console.Write($"Вторая цифра {number} числа - {g}");
+}
+else
+{
+    Console.Write("Введите трехзначное число");
+}
