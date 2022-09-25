5a78dd647cde99dfea0f1f457779a4e1615b2073
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-25 17:46:08 +0300

    Выводит наличие третьей цифры

---
+++ HomeWork02_02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введи число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+string number1 = Convert.ToString(number);
+
+if (number1.Length > 2){
+  Console.WriteLine("Третья цифра -" + number1[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет");
+}
