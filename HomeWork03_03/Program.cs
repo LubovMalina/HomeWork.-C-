5cf16907dd1fef034bf5197b6f3019a230d68b2e
LubovMalina <andreeva.lubov27@gmail.com>
2022-09-25 19:58:15 +0300

    Ввозведение в куб

---
+++ HomeWork03_03/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int step = 1;
+
+while (step < number + 1)
+{
+    double cub = Math.Pow(step,3);
+    Console.WriteLine(cub);
+    step++;
+}
